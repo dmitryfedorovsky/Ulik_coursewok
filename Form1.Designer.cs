0a5963348ba29a4993d56da66701a38ac4c09fe3
dmitryfedorovsky <115088254+dmitryfedorovsky@users.noreply.github.com>
2022-10-14 01:53:03 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Ulik_coursewok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buildbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buildbutton
+            // 
+            this.buildbutton.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buildbutton.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buildbutton.Location = new System.Drawing.Point(53, 44);
+            this.buildbutton.Name = "buildbutton";
+            this.buildbutton.Size = new System.Drawing.Size(204, 126);
+            this.buildbutton.TabIndex = 0;
+            this.buildbutton.Text = "Построить";
+            this.buildbutton.UseVisualStyleBackColor = true;
+            this.buildbutton.UseWaitCursor = true;
+            this.buildbutton.Click += new System.EventHandler(this.Buildbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 226);
+            this.Controls.Add(this.buildbutton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Труба";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buildbutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Ulik_coursewok
+{
+    public partial class Form1 : Form
+    {
+        SldWorks SwApp;
+        ModelDoc2 swModel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Buildbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SwApp = (SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось подключиться к solidworks");
+                return;
+            }
+            if (SwApp.IActiveDoc == null)
+            {
+                SwApp.NewPart();
+            }
+            swModel = SwApp.IActiveDoc2;
+            swModel.SketchManager.Insert3DSketch(true);
+            swModel.SketchManager.CreateLine(0, 0, 0, 0, 0.180, 0);
+            swModel.SketchManager.CreateLine(0, 0.180, 0, -0.25, 0.18, 0);
+            swModel.SketchManager.CreateLine(-0.25, 0.18, 0, -0.25, 0.18, 0.4);
+            swModel.SketchManager.CreateLine(-0.25, 0.18, 0.4, 0, 0.18, 0.4);
+            swModel.SketchManager.CreateLine(0, 0.18, 0.4, 0, 0, 0.4);
+            swModel.Extension.SelectByID2("Line1", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line2", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.01, 1);
+
+            swModel.Extension.SelectByID2("Line2", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line3", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.04, 1);
+
+            swModel.Extension.SelectByID2("Line3", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line4", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.04, 1);
+
+            swModel.Extension.SelectByID2("Line4", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line5", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.01, 1);
+
+            swModel.ClearSelection2(true);
+            swModel.SketchManager.InsertSketch(true);
+
+            swModel.Extension.SelectByID2("Top", "PLANE", 0, 0, 0, false, 0, null, 0);
+            swModel.SketchManager.InsertSketch(true);
+            swModel.ClearSelection2(true);
+            swModel.SketchManager.CreateCenterRectangle(0, 0, 0, 0.006,0.009,0);
+
+            swModel.ClearSelection2(true);
+            swModel.Extension.SelectByID2("Line3", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line4", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.003, 1);
+           
+            swModel.Extension.SelectByID2("Line2", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line3", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.003, 1);
+        
+            swModel.Extension.SelectByID2("Line4", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line1", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.003, 1);
+            
+            swModel.Extension.SelectByID2("Line2", "SKETCHSEGMENT", 0, 0, 0, false, 0, null, 0);
+            swModel.Extension.SelectByID2("Line1", "SKETCHSEGMENT", 0, 0, 0, true, 0, null, 0);
+            swModel.SketchManager.CreateFillet(0.003, 1);
+
+            swModel.ClearSelection2(true);
+            swModel.SketchManager.InsertSketch(true);
+
+            swModel.Extension.SelectByID2("Эскиз1", "SKETCH", 0, 0, 0, false, 1, null, 0);
+            swModel.Extension.SelectByID2("Трехмерный эскиз1", "SKETCH", 0, 0, 0,true, 4, null, 0);
+
+            swModel.FeatureManager.InsertProtrusionSwept4(false, false, 0, false, false, 0, 0, true, 0.002, 0.001, 0, 10, true, true, true, 0, true, false, 0.01, 0);
+
+
+
+
+        }
+    }
+}
